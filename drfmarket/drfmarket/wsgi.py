b55b57b701101dc05e03d4f32a2947dbe8d1a843
034qwe <bnestercuk91@gmail.com>
2023-03-20 15:03:19 +0200

    base django files

---
+++ drfmarket/drfmarket/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for drfmarket project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfmarket.settings')
+
+application = get_wsgi_application()
