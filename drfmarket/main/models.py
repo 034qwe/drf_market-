756af2b6ec4e7bf02cf088f60042a3bdedfbcda7
034qwe <bnestercuk91@gmail.com>
2023-03-20 15:11:20 +0200

    model data

--- drfmarket/main/models.py
+++ drfmarket/main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Articles(models.Model):
+    title=models.CharField('title',max_length=40)
+    price =models.DecimalField()
+    main_text=models.TextField('article')
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    categ = models.ForeignKey('Category_Articles',on_delete=models.PROTECT,) #+= _id
+    slug = models.SlugField(max_length=255,unique=True,db_index=True, verbose_name="URL")
+    user = models.ForeignKey(User,verbose_name='user',on_delete=models.CASCADE)
+
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_slug':self.slug})
+
+    def __str__(self):
+        return self.title
+
+
+class Category_Articles(models.Model):
+    name = models.CharField(max_length=150,db_index=True)
+    slug = models.SlugField(max_length=255,unique=True,db_index=True, verbose_name="URL")
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug':self.slug})
+
+    def __str__(self):
+        return self.name
