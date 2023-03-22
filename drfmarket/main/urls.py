8af09c1e40106bc627d51851eee5ed29103ab7fb
034qwe <bnestercuk91@gmail.com>
2023-03-22 18:45:12 +0200

    url жоской  штуки

--- drfmarket/main/urls.py
+++ drfmarket/main/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('api/v1/articles/', ArticlesApiList.as_view()),
     path('api/v1/articles/<int:pk>/',ArticlesAPIUpdate.as_view()),
     path('api/v1/articlesdelete/<int:pk>/', ArticlesAPIDestroy.as_view()),
-    # path('category/<slug:cat_slug>/',ShowCategoryApiList.as_view() ,name='category')
+    path('category/<slug:cat_slug>/',ShowCategoryApiList.as_view() ,name='category')
 ]
