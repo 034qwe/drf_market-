797e003e22666ef554f6b76ded7e0cc4ca7ee1aa
034qwe <bnestercuk91@gmail.com>
2023-03-30 20:39:10 +0300

    - cart urls

--- drfmarket/main/urls.py
+++ drfmarket/main/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 from main.views import *
 
+
+
+
 urlpatterns = [
     path('articles/', ArticlesApiList.as_view()),
     path('articles/<int:pk>/',ArticlesAPIUpdate.as_view()),
@@ -10,7 +13,4 @@ urlpatterns = [
     path('category/<slug:cat_slug>/',ShowCategoryApiList.as_view() ,name='category'),
     path('all_category/',CategoryArticlesApiList.as_view()),
     path('auth/', include('rest_framework.urls')),
-    path('cart/', CartApiList.as_view(), name='cart-list'),
-    path('cart/<int:pk>/', CartAPIUpdate.as_view(), name='cart-detail'),
-    path('cart/<int:pk>/', CartAPIDestroy.as_view())
 ]
