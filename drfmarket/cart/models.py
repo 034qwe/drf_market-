072fc8b7db944b1b1119982fa6f406ce4dba3cfb
034qwe <bnestercuk91@gmail.com>
2023-03-30 20:36:14 +0300

    Cart Models

---
+++ drfmarket/cart/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+import uuid
+
+
+from main.models import Articles
+
+# Create your models here.
+
+class Cart(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return str(self.id)
+
+class Cartitems(models.Model):
+    # cart = models.ForeignKey(Cart, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(Articles, on_delete=models.CASCADE, blank=True, null=True, related_name='cartitems')
+    quantity = models.IntegerField(default=0)
+    
