a36267467d2bc80ed561196e4cc97ede4982fda1
034qwe <bnestercuk91@gmail.com>
2023-03-30 20:38:02 +0300

    -Cart urls

--- drfmarket/main/models.py
+++ drfmarket/main/models.py
@@ -32,7 +32,3 @@ class Category_Articles(models.Model):
     def __str__(self):
         return self.name
 
-class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Articles)
-    created_at = models.DateTimeField(auto_now_add=True)
