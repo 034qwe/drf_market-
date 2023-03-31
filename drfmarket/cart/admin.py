e3c189fa897d133f61f106bf21b025ca7480db8f
034qwe <bnestercuk91@gmail.com>
2023-03-31 21:24:42 +0300

    register model CartItems

--- drfmarket/cart/admin.py
+++ drfmarket/cart/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Cart
+from .models import Cart,Cartitems
 # Register your models here.
 
 admin.site.register(Cart)
+admin.site.register(Cartitems)
