5d09a14df3d74646b63d0917d9a3e2cc0b3de38c
034qwe <bnestercuk91@gmail.com>
2023-04-22 21:14:24 +0300

    -routers

--- drfmarket/cart/urls.py
+++ drfmarket/cart/urls.py
@@ -5,19 +5,18 @@ from rest_framework import routers
 
 from .views import *
 
-router = routers.DefaultRouter()
-router.register('allcarts', CartAllViewSet)
 
 
 
 urlpatterns = [
-    path('',include(router.urls)),
     path('cartuser/', CartUserAPIList.as_view()),
     path('cartuserdelete/<int:pk>/', CartUserAPIDestroy.as_view()),
     path('carts/',CartAllAPIView.as_view()),
     path('cartadd/',CartAPICreate.as_view()),
     path('cartitemsadd/', CartItemsUserApiAdd.as_view()),
 ]
+
+
 
 
 #я хотел сыграть на кери но зашел в сапер, и теперь сижу в таверне я в таверне.... что ж спою 
