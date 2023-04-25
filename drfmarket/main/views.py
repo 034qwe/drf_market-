af1dee3d5c9c080057999acf79e996b43aa5d3dd
034qwe <bnestercuk91@gmail.com>
2023-04-25 21:28:43 +0300

    CustomPasswordResetView

--- drfmarket/main/views.py
+++ drfmarket/main/views.py
@@ -27,6 +27,7 @@ class ArticlesApiList(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     pagination_class = ArticlesAPIPagination
 
+
 class ShowArticlesApiList(generics.ListAPIView):
     def get_queryset(self):
         pk = self.kwargs.get('pk')
@@ -36,8 +37,6 @@ class ShowArticlesApiList(generics.ListAPIView):
     serializer_class = ArticlesSerializer
 
         
-
-
 class ShowCategoryApiList(generics.ListCreateAPIView):
     def get_queryset(self):
         return Articles.objects.filter(categ__slug=self.kwargs['cat_slug'],).select_related('categ')
@@ -67,10 +66,53 @@ class ArticlesAPIDestroy(generics.RetrieveDestroyAPIView):
 
 
 
-# class ArticlesViewSet(viewsets.ModelViewSet):
-#     # queryset = Articles.objects.all()
-#     serializer_class = ArticlesSerializer
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from djoser.views import PasswordResetView
+from djoser.serializers import PasswordResetSerializer
+from django.utils.translation import ugettext_lazy as _
+from django.core.mail import send_mail
+from django.template import loader
+from django.urls import reverse
+from django.conf import settings
+from djoser import utils
+from djoser.conf import settings as djoser_settings
 
-#     def get_queryset(self):
-#         return Articles.objects.all()
 
+class CustomPasswordResetView(PasswordResetView):
+    serializer_class = PasswordResetSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.user
+        context = {
+            'email': user.email,
+            'domain': request.META['HTTP_HOST'],
+            'site_name': 'Your Site',
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'user': user,
+            'token': default_token_generator.make_token(user),
+            'protocol': 'http',
+        }
+        self.send_reset_password_email(context)
+        return Response({'detail': _('Password reset e-mail has been sent.')})
+    
+    def send_reset_password_email(self, user):
+        context = {
+            'email': user.email,
+            'domain': djoser_settings.get('DOMAIN') or self.request.get_host(),
+            'site_name': djoser_settings.get('SITE_NAME'),
+            'uid': utils.encode_uid(user.pk),
+            'user': user,
+            'token': utils.default_token_generator.make_token(user),
+            'protocol': 'http',
+            'url_reset_password': reverse('password_reset_confirm'),
+        }
+        subject_template_name = 'password_reset_subject.txt'
+        email_template_name = 'password_reset_email.html'
+        subject = loader.render_to_string(subject_template_name, context)
+        subject = ''.join(subject.splitlines())
+        email = loader.render_to_string(email_template_name, context)
+        send_mail(subject, email, settings.DEFAULT_FROM_EMAIL, [user.email])
