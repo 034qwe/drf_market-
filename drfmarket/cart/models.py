58e597883f442c088980ab15fbdc6498c8c705da
034qwe <bnestercuk91@gmail.com>
2023-05-27 22:40:28 +0300

    rework cart and order models

--- drfmarket/cart/models.py
+++ drfmarket/cart/models.py
@@ -15,22 +15,22 @@ class Cart(models.Model):
 
 
     def __str__(self):
-        return str(self.id)
+        return str(self.user_cart)
+
+
+class CartOrder(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    cartorder_date = models.DateTimeField(auto_now_add=True)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE,)
+
+    def __str__(self):
+        return f"{self.owner}"
+
 
 class Cartitems(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE,related_name='items', blank=True, null=True)
+    order = models.ForeignKey(CartOrder,on_delete=models.CASCADE, blank=True, null=True)
     product = models.ForeignKey(Articles, on_delete=models.CASCADE, blank=True, null=True, related_name='cartitems')
     quantity = models.IntegerField(default=1)
 
 
-class CartOrder(models.Model):
-    owner = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='purchase')
-    cartorder_date = models.DateTimeField(auto_now_add=True)
-    product = models.ForeignKey(Cartitems,on_delete=models.CASCADE, null=True,related_name='product_order')
-
-    def __str__(self):
-        return f"cart={self.owner}"
-
-#### короче ідея зробити карт ордер так само як карт геть потім ще одну модеть ордеркарт
-#  як карт ітем тіки вже замість продукта буде екземпляр кат ітема а замість карт карт ордер
-# і квантіті нафіг не треба то буду робити завтра написав щоб не забути!!!
