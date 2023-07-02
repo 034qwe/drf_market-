e22fb8d5678de1a721de91cab6f723bb2eba7a7f
034qwe <bnestercuk91@gmail.com>
2023-07-02 21:48:55 +0300

    test func send_notification_to_telegram

--- drfmarket/cart/tests.py
+++ drfmarket/cart/tests.py
@@ -12,6 +12,8 @@ def send_notification_to_telegram(chat_id):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
     print(requests.get(url).json())
 
-send_notification_to_telegram(chat_id=5512193079)
+
+    
+# send_notification_to_telegram(chat_id=5512193079)
 
 # send_notification_to_telegram(telegram_user=order.user)
