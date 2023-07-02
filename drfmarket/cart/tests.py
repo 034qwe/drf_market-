5b82656adfd3f5e4ecb965a2267b65d18b9a75e8
034qwe <bnestercuk91@gmail.com>
2023-07-02 21:40:49 +0300

    tests

--- drfmarket/cart/tests.py
+++ drfmarket/cart/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+import requests
+
+
+
+def send_notification_to_telegram(chat_id):
+    TOKEN = '6068566463:AAGzmuIKI5CWjgld7jh0_QhYJFkv5MJ2IxE'
+
+    message = "hello from your telegram bot"
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    print(requests.get(url).json())
+
+send_notification_to_telegram(chat_id=5512193079)
+
+# send_notification_to_telegram(telegram_user=order.user)
